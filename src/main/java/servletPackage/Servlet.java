3562dfc8e2ef13bf94b79ac9573affb292cca195
Addison Zheng <32391554+AddisonSubtract@users.noreply.github.com>
2022-11-09 16:18:39 -0600

    first commit

---
+++ src/main/java/servletPackage/Servlet.java
@@ -0,0 +1,56 @@
+package servletPackage;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/*
+ * Servlet implementation class Servlet
+ */
+@WebServlet("/Servlet")
+public class Servlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private Shows myData;
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Servlet() {
+		super();
+		myData = new Shows();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String username = request.getParameter("userName");
+		String password = request.getParameter("passWord");
+		request.setAttribute("userName",username); 
+		request.setAttribute("password", password);
+
+		if(request.getParameter("indexButton")!=null) {
+			if(username.equals("md") && password.equals("pw")) {
+				RequestDispatcher rd =request.getRequestDispatcher("/mainShows.jsp");
+				rd.forward(request,response);
+			} else {
+				RequestDispatcher rd =request.getRequestDispatcher("/index.html");
+				rd.forward(request,response);
+			}
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
